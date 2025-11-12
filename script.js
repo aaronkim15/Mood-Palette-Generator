be69f03c13b55ba6f33c7c29846c85cc0843ed2b
Aaron Kim <aaronkim135@gmail.com>
2025-11-11 23:45:46 -0500

    added color changing based off button clicking

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("calmBtn").addEventListener("click", function() {
+    const myElement = document.getElementById("colourBox");
+    myElement.style.backgroundColor = "lightblue";
+});
+document.getElementById("energeticBtn").addEventListener("click", function() {
+    const myElement = document.getElementById("colourBox");
+    myElement.style.backgroundColor = "orange";
+});
+document.getElementById("tiredBtn").addEventListener("click", function() {
+    const myElement = document.getElementById("colourBox");
+    myElement.style.backgroundColor = "#666699";
+});
