d67bafe6d8997a3526532c4640e79a13062db485
Aaron Kim <aaronkim135@gmail.com>
2025-11-13 13:06:34 -0500

    added copy button and changed the text fonts

--- script.js
+++ script.js
@@ -5,16 +5,31 @@ let tiredArray = ["#666699","#9999ff","#809fff","#b3b3ff","#8080ff"]
 
 document.getElementById("calmBtn").addEventListener("click", function() {
     const myElement = document.getElementById("colourBox");
+    const elementHex = document.getElementById("colourHex");
     const randomVal = Math.floor(Math.random() * calmArray.length);
     myElement.style.backgroundColor = calmArray[randomVal];
+    elementHex.textContent = "HEX Code: " + calmArray[randomVal];
 });
 document.getElementById("energeticBtn").addEventListener("click", function() {
     const myElement = document.getElementById("colourBox");
+    const elementHex= document.getElementById("colourHex");
     const randomVal = Math.floor(Math.random() * energeticArray.length);
     myElement.style.backgroundColor = energeticArray[randomVal];
+    elementHex.textContent = "HEX Code: " + energeticArray[randomVal];
 });
 document.getElementById("tiredBtn").addEventListener("click", function() {
     const myElement = document.getElementById("colourBox");
+    const elementHex = document.getElementById("colourHex");
     const randomVal = Math.floor(Math.random() * tiredArray.length);
     myElement.style.backgroundColor = tiredArray[randomVal];
+    elementHex.textContent = "HEX Code: " + tiredArray[randomVal];
+});
+
+document.getElementById("copyHexBtn").addEventListener("click", function(){
+    const elementHex = document.getElementById("colourHex");
+    const extractedHex = elementHex.textContent.slice(9);
+    if (extractedHex){
+        navigator.clipboard.writeText(extractedHex);
+        alert("Copied to clipboard!")
+    }
 });
